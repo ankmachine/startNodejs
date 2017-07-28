cef2ca84bfbeeb25a5df5622ad3f03cbe5d32db1
sreenivas alapati <sreenivas9alapati@gmail.com>
2017-07-28 17:20:46 +0530

    fixed exports in the router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./server/api');
 
 var app = express();
 
@@ -27,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/api/', require('./server/api'));
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
